fdfa3a37412c95b221ce4ce66ea20f2ae4e5df45
Kyler Axten <60053380+kykyler@users.noreply.github.com>
2023-10-17 21:42:37 -0600

    Add files via upload

---
+++ app/week2/page.js
@@ -0,0 +1,12 @@
+import StudentInfo from '../StudentInfo';
+
+const Week2Page = () => {
+  return (
+    <div>
+      <h1>My Shopping List</h1>
+      <StudentInfo />
+    </div>
+  );
+};
+
+export default Week2Page;
